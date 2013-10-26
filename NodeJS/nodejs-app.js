5238dd076fdc2e849d0b03b0c3764c2119e75485
Brad <bpost>
2013-10-25 20:02:34 -0700

    Adding files and functions

--- NodeJS/nodejs-app.js
+++ NodeJS/nodejs-app.js
@@ -6,8 +6,9 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var http = require('http')
+    , path = require('path')
+    , fs = require('fs');
 
 var app = express();
 
@@ -32,4 +33,36 @@ app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+/*
+ * This handles the moving to the /form on the site
+ */
+app.get('/form', function (req, result) {
+    fs.readFile('./form.html', function (error, content) {
+        if (error) {
+            result.writeHead(500);
+            result.end();
+        }
+        else {
+            result.writeHead(200, { 'Content-Type': 'text/html' });
+            result.end(content, 'utf-8');
+        }
+    });
+});
+
+/*
+ * This handles the /signup post request that is triggered by
+ * the form.html input button
+ */
+app.post('/signup', function(inputs, results) {
+    var username = inputs.body.username;
+    var password = inputs.body.password;
+
+    Users.addUser(username, password, function (error, user) {
+        if (error)
+            throw error;
+
+        results.redirect('/form');
+    });
 });
