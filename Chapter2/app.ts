d69b2178fee0c363b20e7af4b74db58ba90dabd0
Brad <bpost>
2013-11-01 11:36:37 -0700

    Added new file as updated code

--- Chapter2/app.ts
+++ Chapter2/app.ts
@@ -1,28 +1,9 @@
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+//
+// Nockmarket
+//
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
+'use strict';
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
-}
-
-window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-};
+var exchangeData = {},
+    exch = require('./lib/exchange'),
+    nocklib = require('./lib/nocklib');
